d32f53d6169188af4530f7c6665169aad6db9057
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-18 19:15:36 +0500

    Comment.css to module

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,23 +1,23 @@
-import './Comment.css';
+import styles from './Comment.module.css';
 
 import { type CommentData } from '../types.d.tsx';
 
 function Comment({ username, content, avatarUrl }: CommentData) {
     return (
         <>
-            <div className="comment">
-                <div className="comment-left">
+            <div className={styles.comment}>
+                <div className={styles.commentLeft}>
                     <img
                         src={avatarUrl}
                         alt={`${username}'s avatar`}
-                        className="avatar"
+                        className={styles.avatar}
                     />
                 </div>
-                <div className="comment-right">
-                    <p className='username'>{username}</p>
-                    <p className='content'>{content}</p>
+                <div className={styles.commentRight}>
+                    <p className={styles.username}>{username}</p>
+                    <p className={styles.content}>{content}</p>
                 </div>
-                <div className="comment-footer">
+                <div className={styles.commentFooter}>
                     
                 </div>
             </div>
