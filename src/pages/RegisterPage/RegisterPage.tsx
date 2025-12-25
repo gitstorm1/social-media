24aab44b337943d74bf1d0487fd25071a95025b7
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-12-26 00:53:28 +0500

    Create basic structure of RegisterPage

---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import styles from "./RegisterPage.module.css";
+
+function RegisterPage() {
+    return (
+        <div className={styles.mainContent}>
+
+        </div>
+    );
+}
+
+export default RegisterPage;
